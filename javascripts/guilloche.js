b745d166d3e9ed62aed8993b4133374bcafff70b
Jason Long <jlong@github.com>
2013-11-21 12:01:34 -0800

    sha-driven colors

--- javascripts/guilloche.js
+++ javascripts/guilloche.js
@@ -4,25 +4,37 @@ var guilloche = function(canvas, string){
       minorROffset        = parseInt(string.substr(9, 1), 16) / 10,
       angleOffset         = parseInt(string.substr(10, 1), 16) / 10,
       radiusEffectOffset  = parseInt(string.substr(11, 1), 16) / 1.5,
-      stepsOffset         = parseInt(string.substr(12, 2), 16);
+      stepsOffset         = parseInt(string.substr(12, 2), 16),
+      hueOffset           = parseInt(string.substr(14, 3), 16),
+      satOffset           = parseInt(string.substr(17, 1), 16);
 
   var ctx                  = canvas.getContext('2d'),
       size                 = {x: canvas.offsetWidth, y: canvas.offsetHeight},
       halfSize             = {x: size.x / 2, y: size.y / 2},
-      majorR               = 379.5 - majorROffset,
+      majorR               = 379.6 - majorROffset,
       minorR               = 50 - minorROffset,
       angleMultiplier      = 50 - angleOffset,
       radiusEffectConstant = 250 - radiusEffectOffset,
       steps                = 1250 - stepsOffset,
       centerPoint          = { x: 150, y: 42 },
       color                = 'rgba(255,255,255, 0.06)',
-      globalAlpha          = 1.0;
+      globalAlpha          = 1.0,
+      baseBGColor          = Color("#933c3c");
+
+
+  mappedHueOffset = map(hueOffset, 0, 4095, 0, 360);
+  baseBGColor.hue(360 - mappedHueOffset);
+  if (satOffset % 2) {
+    baseBGColor.saturate(satOffset / 10);
+  }
+  else {
+    baseBGColor.desaturate(satOffset / 10);
+  }
 
   ctx.globalAlpha = globalAlpha;
   ctx.clearRect(0, 0, size.x, size.y);
-  // console.log("#" + string.substring(4, 10));
-  // ctx.fillStyle = "#" + string.substring(4, 10);
-  // ctx.fillRect(0, 0, size.x, size.y);
+  ctx.fillStyle = baseBGColor.rgbString();
+  ctx.fillRect(0, 0, size.x, size.y);
 
   var diff = majorR - minorR,
       s = diff / minorR,
@@ -52,10 +64,9 @@ var guilloche = function(canvas, string){
 
 };
 
-// def map(value, v_min, v_max, d_min, d_max) # v for value, d for desired
-//   v_value = value.to_f # so it returns float
-
-//   v_range = v_max - v_min
-//   d_range = d_max - d_min
-//   d_value = (v_value - v_min) * d_range / v_range + d_min
-// end
+function map(value, v_min, v_max, d_min, d_max) {
+  v_value = parseFloat(value);
+  v_range = v_max - v_min;
+  d_range = d_max - d_min;
+  return (v_value - v_min) * d_range / v_range + d_min;
+}
