25fdfd87082725da581667d5c1abd6bfb6dde02f
Jason Long <jlong@github.com>
2013-12-05 10:08:19 -0500

    only init ToC on article page

--- javascripts/application.js
+++ javascripts/application.js
@@ -20,7 +20,6 @@ $(function(){
     guillocheSVG(svgCanvas, {string: titleSHA, type: "article", center: center});
   };
 
-
   $('.js-guide-listing').each(function() {
     renderGuideListing($(this));
   });
@@ -40,77 +39,76 @@ $(function(){
     }
   });
 
-  tableOfContents($('.js-toc'))
 
-  //
-  // Scroll Handlers
-  //
+  // Initalize the ToC if we're on an article page
+  if ($('js-toc').length) {
+    tableOfContents($('.js-toc'));
 
-  // Table of contents
-  var toc = $('.toc')
-  var tocOffset = toc.offset().top
-  var tocPadding = 20
+    var toc = $('.js-toc');
+    var tocOffset = toc.offset().top;
+    var tocPadding = 20;
 
-  var tocSections = $('.toc-item')
-  var tocSectionOffsets = []
+    var tocSections = $('.toc-item');
+    var tocSectionOffsets = [];
 
-  // Calculates the toc section offsets, which can change as images get loaded
-  var calculateTocSections = function(){
-    tocSectionOffsets = []
-    tocSections.each(function(index, section){
-      tocSectionOffsets.push(section.offsetTop)
-    })
-  }
-  calculateTocSections()
-  $(window).bind('load', calculateTocSections)
-
-  var highlightTocSection = function(){
-    var highlightIndex = 0
-    $.each(tocSectionOffsets, function(index, offset){
-      if (window.scrollY > offset - 20){
-        highlightIndex = index
-      }
-    })
-    highlightIndex += 1
-    $('ol.toc .active').removeClass('active')
-    $('ol.toc li:nth-child(' + highlightIndex + ') a').addClass('active')
-  }
-  highlightTocSection()
-
-  var didScroll = false;
-  $(window).scroll(function() {
-      didScroll = true
-  })
-
-  setInterval(function() {
-    if (didScroll) {
-      didScroll = false;
-
-      if (window.scrollY > tocOffset - tocPadding)
-        toc.addClass('sticky')
-      else
-        toc.removeClass('sticky')
+    // Calculates the toc section offsets, which can change as images get loaded
+    var calculateTocSections = function(){
+      tocSectionOffsets = [];
+      tocSections.each(function(index, section){
+        tocSectionOffsets.push(section.offsetTop);
+      })
     }
-    highlightTocSection()
-  }, 100);
+    calculateTocSections();
+    $(window).bind('load', calculateTocSections);
+
+    var highlightTocSection = function(){
+      var highlightIndex = 0;
+      $.each(tocSectionOffsets, function(index, offset){
+        if (window.scrollY > offset - 20){
+          highlightIndex = index;
+        }
+      })
+      highlightIndex += 1;
+      $('ol.toc .active').removeClass('active');
+      $('ol.toc li:nth-child(' + highlightIndex + ') a').addClass('active');
+    }
+    highlightTocSection();
+
+    var didScroll = false;
+    $(window).scroll(function() {
+      didScroll = true;
+    })
+
+    setInterval(function() {
+      if (didScroll) {
+        didScroll = false;
+
+        if (window.scrollY > tocOffset - tocPadding)
+          toc.addClass('sticky');
+        else
+          toc.removeClass('sticky');
+      }
+      highlightTocSection();
+    }, 100);
+  }
 })
 
 
 // Generates a table of contents based on a.toc-item elements throughout the
 // page. Follows allong via scroll and
 var tableOfContents = function($listContainer) {
-  if ($listContainer.length === 0) return
+  if ($listContainer.length === 0) return;
 
   $('.toc-item').each(function(index, chapterAnchor) {
-    $chapterAnchor = $(chapterAnchor)
+    $chapterAnchor = $(chapterAnchor);
     var listLink = $('<a>')
     .attr('href', '#' + $chapterAnchor.attr('id'))
     .text($chapterAnchor.attr('title'))
-    .bind('click', scrollTo)
+    .bind('click', scrollTo);
 
-    var listItem = $('<li>').append(listLink)
+    var listItem = $('<li>').append(listLink);
 
-    $listContainer.append(listItem)
+    $listContainer.append(listItem);
   })
 }
 
