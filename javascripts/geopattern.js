e4183e167b5a1a3cb4799979947f388b333635c0
Jason Long <jlong@github.com>
2013-12-27 16:38:45 -0500

    refactor functions

--- javascripts/geopattern.js
+++ javascripts/geopattern.js
@@ -2,40 +2,54 @@
 
     $.fn.geopattern = function(options) {
       return this.each(function() {
-        var sha = $(this).data('title-sha');
-        var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svg.setAttribute("width", "100");
-        svg.setAttribute("height", "100");
-        var s = Snap(svg);
-        var bigCircle = s.circle(50, 50, 50);
+        var container = $(this);
+        var sha       = $(this).data('title-sha');
+        var s         = Snap();
 
-        var b64 = 'data:image/svg+xml;base64,'+window.btoa(s.toString());
-        var url = 'url("' + b64 + '")';
+        setBGColor(s, sha, container);
+        geoSquares(s, sha);
+        renderPattern(s, container);
+      });
 
-        var hueOffset = parseInt(sha.substr(14, 3), 16);
-        var satOffset = parseInt(sha.substr(17, 1), 16);
-        var baseBGColor = Snap.getRGB("#933c3c");
-
+      function setBGColor(s, sha, container) {
+        var hueOffset       = parseInt(sha.substr(14, 3), 16);
+        var satOffset       = parseInt(sha.substr(17, 1), 16) / 100;
+        var bgRGB           = Snap.getRGB("#933c3c");
         var mappedHueOffset = map(hueOffset, 0, 4095, 0, 1);
-        var baseBGColorHSL = Snap.rgb2hsb(baseBGColor.r, baseBGColor.g, baseBGColor.b);
-        baseBGColorHSL.h = 1 - mappedHueOffset;
+        var bgHSL           = Snap.rgb2hsb(bgRGB.r, bgRGB.g, bgRGB.b);
+        bgHSL.h             = 1 - mappedHueOffset;
 
         if (satOffset % 2) {
-          baseBGColorHSL.s += satOffset / 100;
+          bgHSL.s += satOffset;
         }
         else {
-          baseBGColorHSL.s -= satOffset / 100;
+          bgHSL.s -= satOffset;
         }
-        $(this).css('background-color', Snap.hsb2rgb(baseBGColorHSL.h, baseBGColorHSL.s, baseBGColorHSL.b).hex);
-        $(this).css('background-image', url);
-      });
+        $(container).css('background-color', Snap.hsb2rgb(bgHSL.h, bgHSL.s, bgHSL.b).hex);
+      }
+
+      function geoSquares(s, sha) {
+        s.attr({
+          width:  '200px',
+          height: '200px'
+        });
+        var bigCircle = s.circle(50, 50, 50);
+      }
+
+      function renderPattern(s, container) {
+        var b64 = 'data:image/svg+xml;base64,'+window.btoa(s.toString());
+        var url = 'url("' + b64 + '")';
+        $(container).css('background-image', url);
+      }
+
+      function map(value, v_min, v_max, d_min, d_max) {
+        v_value = parseFloat(value);
+        v_range = v_max - v_min;
+        d_range = d_max - d_min;
+        return (v_value - v_min) * d_range / v_range + d_min;
+      }
+
     };
 
-    function map(value, v_min, v_max, d_min, d_max) {
-      v_value = parseFloat(value);
-      v_range = v_max - v_min;
-      d_range = d_max - d_min;
-      return (v_value - v_min) * d_range / v_range + d_min;
-    }
 
 }(jQuery));
