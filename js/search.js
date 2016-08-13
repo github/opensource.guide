3adf6fc0a5c8e5160810d001b48c9ed315b30274
Brandon Keepers <bkeepers@github.com>
2016-08-13 15:32:41 -0500

    Add search with lunr.js

---
+++ js/search.js
@@ -0,0 +1,56 @@
+// http://jekyll.tips/jekyll-casts/jekyll-search-using-lunr-js/
+
+(function() {
+  var store = window.store;
+  var searchTerm = getQueryVariable('q');
+
+  if (searchTerm) {
+    // Set value of search box to search parameter
+    document.getElementById('search-box').setAttribute("value", searchTerm);
+
+    // Initalize lunr
+    var idx = window.x = lunr(function () {
+      this.ref('url');
+      this.field('title', { boost: 10 });
+      this.field('content');
+    });
+
+    // Add content to index
+    for(var id in store) {
+      idx.add(store[id]);
+    }
+
+    // Perform the search
+    var results = idx.search(searchTerm);
+    displaySearchResults(results);
+  }
+
+  function displaySearchResults(results) {
+    var searchResults = document.getElementById('search-results');
+
+    if (results.length) {
+      var appendString = '';
+
+      results.forEach(function(result) {
+        appendString += store[result.ref].html;
+      });
+
+      searchResults.innerHTML = appendString;
+    } else {
+      searchResults.innerHTML = '<li>No results found</li>';
+    }
+  }
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+      }
+    }
+  }
+})();
