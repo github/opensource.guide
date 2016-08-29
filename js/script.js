3c5713b4084a5f0151785ec42303c933de3f1d6f
Sophie Shepherd <sophshep@github.com>
2016-08-29 17:11:43 -0500

    Start of off-canvas menu

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function toggleNav() {
+  if ($('.site-wrapper').hasClass('show-nav')) {
+      // Do things on Nav Close
+      $('.site-wrapper').removeClass('show-nav');
+  } else {
+      // Do things on Nav Open
+      $('.site-wrapper').addClass('show-nav');
+  }
+  //$('#site-wrapper').toggleClass('show-nav');
+}
+
+$(function() {
+    // Toggle Nav on Click
+    $('.toggle-nav').click(function() {
+        // Calling a function in case you want to expand upon this.
+        toggleNav();
+    });
+});
