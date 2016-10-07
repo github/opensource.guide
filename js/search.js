657d86f1537a7691516a6d2dfe5ed0222bfb5396
Brandon Keepers <bkeepers@github.com>
2016-10-07 08:47:14 -0500

    Move lunr to web worker

--- js/search.js
+++ js/search.js
@@ -2,70 +2,38 @@
 layout: false
 ---
 
-window.store = {
-{% for article in site.articles %}
-  {% capture html %}{% include search-result.html article=article %}{% endcapture %}
-  "{{ article.url | xml_escape }}": {
-    "title": "{{ article.title | xml_escape }}",
-    "content": {{ article.content | markdownify | strip_html | strip_newlines | jsonify }},
-    "url": "{{ article.url | xml_escape }}",
-    "html": {{ html | jsonify }}
-  }{% unless forloop.last %},{% endunless %}
-{% endfor %}
-};
+var searchWorker = new Worker("{{ "/js/search_worker.js" | prepend: site.baseurl }}");
 
-// http://jekyll.tips/jekyll-casts/jekyll-search-using-lunr-js/
-(function() {
-  var store = window.store;
-  var searchTerm = getQueryVariable('q');
+searchWorker.addEventListener("message", function(e) {
+  displaySearchResults(e.data);
+});
 
-  if (searchTerm) {
-    // Set value of search box to search parameter
-    document.getElementById('search-box').setAttribute("value", searchTerm);
+var searchTerm = getQueryVariable('q');
+if (searchTerm) {
+  // Set value of search box to search parameter
+  document.getElementById('search-box').setAttribute("value", searchTerm);
+  searchWorker.postMessage(searchTerm);
+}
 
-    // Initalize lunr
-    var idx = window.x = lunr(function () {
-      this.ref('url');
-      this.field('title', { boost: 10 });
-      this.field('content');
-    });
+function displaySearchResults(results) {
+  var searchResults = document.getElementById('search-results');
 
-    // Add content to index
-    for(var id in store) {
-      idx.add(store[id]);
-    }
-
-    // Perform the search
-    var results = idx.search(searchTerm);
-    displaySearchResults(results);
+  if (results.length) {
+    searchResults.innerHTML = results.join("");
+  } else {
+    searchResults.innerHTML = '<li>No results found</li>';
   }
+}
 
-  function displaySearchResults(results) {
-    var searchResults = document.getElementById('search-results');
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
 
-    if (results.length) {
-      var appendString = '';
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
 
-      results.forEach(function(result) {
-        appendString += store[result.ref].html;
-      });
-
-      searchResults.innerHTML = appendString;
-    } else {
-      searchResults.innerHTML = '<li>No results found</li>';
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
     }
   }
-
-  function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
-
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split('=');
-
-      if (pair[0] === variable) {
-        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
-      }
-    }
-  }
-})();
+}
