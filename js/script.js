e389366473a0ddfacd6dc6fb29cdbcefaa931265
Yevhen Amelin <yevhen.amelin@gmail.com>
2017-02-16 21:06:20 +0200

    Fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 $(function() {
+    $(document).click(function() {
+      // Hiding ToC
+      $('.toc-trigger').removeClass('toc-open');
+      $('.toc-list').removeClass('is-shown');
+    });
+
     // Toggle Nav on Click
-    $('.toc-trigger').click(function() {
+    $('.toc-trigger').click(function(e) {
+        // Prevent bubbling event for proper hiding
+        e.stopPropagation();
         // Calling a function in case you want to expand upon this.
         $(this).toggleClass('toc-open');
         $('.toc-list').toggleClass('is-shown');
