8b935d84639066fa659d7cf7ccdd81c60c9427c0
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-07-21 13:07:44 -0500

    Test deploy in subdir

--- assets/js/locale.js
+++ assets/js/locale.js
@@ -9,7 +9,7 @@ function loadLanguage(lang) {
   if (lang === "en") {
     url = base_pathname
   } else {
-    url = "/" + lang + base_pathname
+    url = base_pathname + lang
   }
   window.location.assign(url);
 }
