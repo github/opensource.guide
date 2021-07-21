0eaa9f80b3d25a99d7dfe3a11fb68fe2d3f475c9
RawandDev <rawand.200091384037@gmail.com>
2021-07-21 13:50:02 +0300

    modify index.js to es6 syntax

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@
 {% include_relative toc.js %}
 {% include_relative locale.js %}
 
-(function() {
+(() => {
   var selector = '.article-body h2, .article-body h3, .article-body h4, .article-body h5';
   anchors.options = {
     placement: 'left'
@@ -16,7 +16,7 @@
   $(selector).wrapInner('<span/>');
 })();
 
-(function() {
+(() => {
   var FRIDAY = 5;
   var today = new Date();
   if (FRIDAY == today.getDay()) {
