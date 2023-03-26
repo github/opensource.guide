e988872133f91fb65d677c5753343eb3598dda89
Nathaniel Handan <87228776+Tinny-Robot@users.noreply.github.com>
2023-03-26 11:45:01 +0100

    Improve site method readability
    Add documentation to site-related methods
    
    This commit adds internal documentation to the methods in the Helper module that relate to the Jekyll site. The documentation explains what each method does and how it should be used. The code is also formatted according to the Ruby style guide for improved readability. These changes should make it easier for other developers to understand and work with the site-related code.

--- test/helper.rb
+++ test/helper.rb
@@ -1,32 +1,40 @@
+# Import the necessary libraries.
 require "minitest/autorun"
 require "jekyll"
 
+# Define a module to store site-related variables and methods.
 module Helper
   class << self
+    # Allow access to configuration and site variables from outside the module.
     attr_accessor :config, :site
   end
 end
 
+# Returns the path to the directory containing the source files.
 def source
   File.expand_path("../", File.dirname(__FILE__))
 end
 
+# Returns the configuration for the Jekyll site, loading it if not yet defined.
 def config
+  # Use the existing configuration if it already exists.
   Helper.config ||= Jekyll.configuration("source" => source)
 end
 
-def docs
-  (site.pages + site.collections["articles"].docs)
-end
-
+# Returns an array containing all the pages in the Jekyll site.
 def pages
-  docs.map(&:to_liquid)
+  # Get all the documents in the "articles" collection and add them to the list of pages.
+  (site.pages + site.collections["articles"].docs).map(&:to_liquid)
 end
 
+# Returns the Jekyll site object, creating it if not yet defined.
 def site
+  # Use the existing site object if it already exists.
   Helper.site ||= begin
     site = Jekyll::Site.new(config)
+    # Reset the site to its initial state, including clearing the cache.
     site.reset
+    # Read in all the site data from the source files.
     site.read
     site
   end
